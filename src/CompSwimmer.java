552ca7278065621e2d7b8c6c83fe135c4733977a
Yakoob1884 <vivja.aty@gmail.com>
2025-05-09 14:45:34 +0200

    CompSwimmer updates

--- src/CompSwimmer.java
+++ src/CompSwimmer.java
@@ -1,12 +1,20 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.*;
 
-public class CompSwimmer extends NonCompSwimmer implements CompetitionSwimmer{
+public class CompSwimmer extends NonCompSwimmer implements CompetitionSwimmer {
     //Attribute
 
+    EnumSet<SwimmingDiscipline> discipline;
+    ArrayList<Competition> compTime;
+    Training trainingTime;
 
-    public CompSwimmer (String firstName, String lastName, LocalDate birthday, boolean isActive, boolean isPaid){
+
+    public CompSwimmer(String firstName, String lastName, LocalDate birthday, boolean isActive, boolean isPaid, Training trainingTime) {
         super(firstName, lastName, birthday, isActive, isPaid);
-
+        this.discipline = EnumSet.noneOf(SwimmingDiscipline.class);
+        this.compTime = new ArrayList<>();
+        this.trainingTime = trainingTime;
     }
 
 }
--- src/CompetitionSwimmer.java
+++ src/CompetitionSwimmer.java
@@ -1,3 +1,3 @@
-public interface CompetitionSwimmer extends Swimmer{
+public interface CompetitionSwimmer extends Swimmer {
 
 }
