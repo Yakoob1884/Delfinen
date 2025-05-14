d00da7cf715084b9c0b48c47b7a7101038b4c3c0
JB4000 <jabi0002@stud.kea.dk>
2025-05-14 11:05:27 +0200

    ændret så controller også har en scanner

--- src/main/java/com/delfin/main/Main.java
+++ src/main/java/com/delfin/main/Main.java
@@ -4,14 +4,18 @@ package delfin.main;
 
 import delfin.controller.AppController;
 
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        AppController controller = new AppController("collectionBackup.ser");
-        UI uiConsoleMenu = new UI(controller);
+        Scanner scanner = new Scanner(System.in);
+
+
+        AppController controller = new AppController("collectionBackup.ser", scanner);
+        UI uiConsoleMenu = new UI(controller, scanner);
 
         uiConsoleMenu.menuOptions();
 
